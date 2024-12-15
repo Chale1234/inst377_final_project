41bd61f84f2c524a986da8a7a84ec23fa4e7d0f7
chale <enchalewtamiru@gmail.com>
2024-12-15 16:24:21 -0500

    final sample

--- index.js
+++ index.js
@@ -165,5 +165,5 @@ app.post('/weather', async (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Express app listening on port:${port}`);
+    console.log(`Express app listening on port:${host}`);
 });
